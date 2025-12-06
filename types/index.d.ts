8d8c1a6e6317b93b2edbca79282f31fdf70383c0
Leonardo Benicio <parquet-rubbles-08@icloud.com>
2025-12-06 14:01:56 -0300

    feat: add TypeScript declarations for CSS modules and update tsconfig for arbitrary extensions

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,3 +4,7 @@ declare module "*.css" {
   const content: { [className: string]: string };
   export default content;
 }
+
+declare module "@styles/globals.css";
+declare module "@styles/markdown.css";
+declare module "@styles/syntax.css";
